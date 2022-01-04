7fa15b9dec7b512ebba277703b83408672220372
Quaid Bartolomei <quaid.bartolomei@gmail.com>
2022-01-03 23:02:04 -0800

    chore: sort eslint rules and disable consistent-return

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,17 +7,15 @@ module.exports = {
     'airbnb-typescript/base',
     'prettier',
   ],
+
   rules: {
-    'import/no-extraneous-dependencies': [
-      'error',
-      {
-        devDependencies: ['**/*.{test,spec}.ts', 'test/**/*'],
-      },
-    ],
     'no-console': 'off',
     'import/prefer-default-export': 'off',
     'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
     'no-promise-executor-return': 'off',
+    'consistent-return': 'off',
+
+    // naming conventions
     camelcase: 'off',
     '@typescript-eslint/naming-convention': [
       'error',
@@ -26,6 +24,16 @@ module.exports = {
         format: ['camelCase', 'UPPER_CASE', 'snake_case'],
       },
     ],
+
+    // allow test files to use dev deps
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: ['**/*.{test,spec}.ts', 'test/**/*'],
+      },
+    ],
+
+    // import sorting
     'import/order': [
       'error',
       {
@@ -46,17 +54,21 @@ module.exports = {
       },
     ],
   },
+
   env: {
     es2021: true,
     node: true,
     jest: true,
   },
+
   parser: '@typescript-eslint/parser',
+
   parserOptions: {
     ecmaVersion: 13,
     sourceType: 'module',
     project: ['./tsconfig.json'],
   },
+
   settings: {
     'import/resolver': {
       node: {
